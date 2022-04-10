2a45d793903bfffd294a8a4ac6c9d7164bdd47c0
FFEDSerj <ffedchik@gmail.com>
2022-04-10 18:05:03 +0300

    implemented 2 ways of singing in, with google popup and google redirect

--- src/routes/signin/sign-in.component.jsx
+++ src/routes/signin/sign-in.component.jsx
@@ -1,18 +1,34 @@
+import { useEffect } from "react";
+import { getRedirectResult } from "firebase/auth";
 import {
+  auth,
   singInWithGooglePopup,
+  singInWithGoogleRedirect,
   createUserDocumentFromAuth,
 } from "../../utils/firebase/firebase.utils";
 
 const SignIn = () => {
+  useEffect(() => {
+    const fetchUser = async () => {
+      const response = await getRedirectResult(auth);
+      if (response) {
+        const userDocRef = await createUserDocumentFromAuth(response.user);
+      }
+    };
+    fetchUser();
+  }, []);
+
   const logGoogleUser = async () => {
     const { user } = await singInWithGooglePopup();
-    const userDocRef = await createUserDocumentFromAuth(user);
   };
 
   return (
     <div>
       <h1>Sign in page</h1>
       <button onClick={logGoogleUser}>Sign In with Google Popup</button>
+      <button onClick={singInWithGoogleRedirect}>
+        Sign In with Google Redirect
+      </button>
     </div>
   );
 };
--- src/utils/firebase/firebase.utils.js
+++ src/utils/firebase/firebase.utils.js
@@ -18,14 +18,17 @@ const firebaseConfig = {
 
 const firebaseApp = initializeApp(firebaseConfig);
 
-const provider = new GoogleAuthProvider();
+const googleProvider = new GoogleAuthProvider();
 
-provider.setCustomParameters({
+googleProvider.setCustomParameters({
   prompt: "select_account",
 });
 
 export const auth = getAuth();
-export const singInWithGooglePopup = () => signInWithPopup(auth, provider);
+export const singInWithGooglePopup = () =>
+  signInWithPopup(auth, googleProvider);
+export const singInWithGoogleRedirect = () =>
+  signInWithRedirect(auth, googleProvider);
 
 export const db = getFirestore();
 
