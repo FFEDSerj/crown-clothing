5260b035952455db00a76a555ea96493016a080e
FFEDSerj <ffedchik@gmail.com>
2022-04-10 15:12:58 +0300

    Added react router dom to the app and prepared default routes for start

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
+import { BrowserRouter } from "react-router-dom";
 import "./index.scss";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
@@ -8,7 +9,11 @@ const container = document.getElementById("root");
 
 const root = createRoot(container);
 
-root.render(<App tab="home" />);
+root.render(
+  <BrowserRouter>
+    <App tab="home" />
+  </BrowserRouter>
+);
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
