8a58d5f1d188f26990b78af4b964cf5af0a5397c
FFEDSerj <ffedchik@gmail.com>
2022-04-10 20:22:02 +0300

    created the sign up component, form input component, button component

--- src/routes/signin/sign-in.component.jsx
+++ src/routes/signin/sign-in.component.jsx
@@ -1,34 +1,20 @@
-import { useEffect } from "react";
-import { getRedirectResult } from "firebase/auth";
 import {
-  auth,
   singInWithGooglePopup,
-  singInWithGoogleRedirect,
   createUserDocumentFromAuth,
 } from "../../utils/firebase/firebase.utils";
+import SignUpForm from "../../components/sign-up-form/sign-up-form.component";
 
 const SignIn = () => {
-  useEffect(() => {
-    const fetchUser = async () => {
-      const response = await getRedirectResult(auth);
-      if (response) {
-        const userDocRef = await createUserDocumentFromAuth(response.user);
-      }
-    };
-    fetchUser();
-  }, []);
-
   const logGoogleUser = async () => {
     const { user } = await singInWithGooglePopup();
+    const userDocRef = await createUserDocumentFromAuth(user);
   };
 
   return (
     <div>
-      <h1>Sign in page</h1>
+      <h1>Sign In page</h1>
       <button onClick={logGoogleUser}>Sign In with Google Popup</button>
-      <button onClick={singInWithGoogleRedirect}>
-        Sign In with Google Redirect
-      </button>
+      <SignUpForm />
     </div>
   );
 };
--- src/utils/firebase/firebase.utils.js
+++ src/utils/firebase/firebase.utils.js
@@ -3,6 +3,7 @@ import {
   getAuth,
   signInWithRedirect,
   signInWithPopup,
+  createUserWithEmailAndPassword,
   GoogleAuthProvider,
 } from "firebase/auth";
 import { getFirestore, doc, getDoc, setDoc } from "firebase/firestore";
@@ -32,7 +33,11 @@ export const singInWithGoogleRedirect = () =>
 
 export const db = getFirestore();
 
-export const createUserDocumentFromAuth = async (userAuth) => {
+export const createUserDocumentFromAuth = async (
+  userAuth,
+  additionalInfo = {}
+) => {
+  if (!userAuth) return;
   const userDocRef = doc(db, "users", userAuth.uid);
 
   const userSnapshot = await getDoc(userDocRef);
@@ -48,6 +53,7 @@ export const createUserDocumentFromAuth = async (userAuth) => {
         displayName,
         email,
         createdAt,
+        ...additionalInfo,
       });
     } catch (err) {
       console.log("error creating the user", err.message);
@@ -56,4 +62,10 @@ export const createUserDocumentFromAuth = async (userAuth) => {
   // if user data exists
 
   return userDocRef;
+};
+
+export const createAuthUserWithEmailAndPassword = async (email, password) => {
+  if (!email || !password) return;
+
+  return await createUserWithEmailAndPassword(auth, email, password);
 };
