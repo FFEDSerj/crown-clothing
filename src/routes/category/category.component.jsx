e852f277cad1faeb91498af62eefe34747444948
FFEDSerj <ffedchik@gmail.com>
2022-04-25 19:37:28 +0300

    added redux-thunk and rewrote the store. next was added a spinner for isLoading effect

--- src/routes/category/category.component.jsx
+++ src/routes/category/category.component.jsx
@@ -2,13 +2,17 @@ import { useState, useEffect } from "react";
 import { useSelector } from "react-redux";
 import { useParams } from "react-router-dom";
 import ProductCard from "../../components/product-card/product-card.component";
-
-import { selectCategoriesMap } from "../../store/categories/category.selector";
+import Spinner from "../../components/spinner/spinner.component";
+import {
+  selectCategoriesIsLoading,
+  selectCategoriesMap,
+} from "../../store/categories/category.selector";
 import "./category.styles.scss";
 
 const Category = () => {
   const { category } = useParams();
   const categoriesMap = useSelector(selectCategoriesMap);
+  const isLoading = useSelector(selectCategoriesIsLoading);
   const [products, setProducts] = useState([]);
 
   useEffect(() => {
@@ -18,11 +22,15 @@ const Category = () => {
   return (
     <>
       <h2 className="category-title">{category}</h2>
-      <div className="category-container">
-        {products?.map((product) => (
-          <ProductCard key={product.id} product={product} />
-        ))}
-      </div>
+      {isLoading ? (
+        <Spinner />
+      ) : (
+        <div className="category-container">
+          {products?.map((product) => (
+            <ProductCard key={product.id} product={product} />
+          ))}
+        </div>
+      )}
     </>
   );
 };
--- src/store/store.js
+++ src/store/store.js
@@ -2,20 +2,22 @@ import { compose, createStore, applyMiddleware } from "redux";
 import { persistStore, persistReducer } from "redux-persist";
 import storage from "redux-persist/lib/storage";
 import logger from "redux-logger";
+import thunk from "redux-thunk";
 
 import { rootReducer } from "./root.reducer";
 
 const persistConfig = {
   key: "root",
   storage,
-  blackList: ["user"],
+  whiteList: ["cart"],
 };
 
 const persistedReducer = persistReducer(persistConfig, rootReducer);
 
-const middleWares = [process.env.NODE_ENV === "development" && logger].filter(
-  Boolean
-);
+const middleWares = [
+  process.env.NODE_ENV === "development" && logger,
+  thunk,
+].filter(Boolean);
 // chrome redux devtools
 const composedEnhancer =
   (process.env.NODE_ENV !== "production" &&
